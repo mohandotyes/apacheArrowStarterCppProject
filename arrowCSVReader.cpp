67508a8a1b807e023b34666094846c2b42da60d1
DotYes <mohandotyes@gmail.com>
2020-06-03 13:23:03 +0530

    How to build arrow array

--- arrowCSVReader.cpp
+++ arrowCSVReader.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <arrow/api.h>
 
 int main(){
 
-	std::cout << "hello wolrd\n" << '\n';
-	std::cout << "hello mohan\n" << '\n';
+	arrow::Int64Builder builder;
+	builder.Append(1);
+	builder.Append(2);
+	builder.Append(3);
+
+	std::shared_ptr<arrow::Array> array;
+	arrow::Status st = builder.Finish(&array);
+	if(!st.ok()){
+		printf("ERROR\n");
+	}
+	else{
+		printf("Done\n");
+	}
 
 	return 0;
 }
